038c2b770bcf262dfa4cf22d94eaad8c44763b6c
Aygyun Ahmed <Aygunahmed7@gmail.com>
2024-01-06 23:58:47 +0200

    Data binding types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  
+  template: `
+    
+  `,
+  /*
+  template: `
+  Text Interpolation
+  <h1>{{title}}</h1>
+ 
+  Property Binding
+  <input type="text" [placeholder]="title">
+  <app-home [pageName]=""></app-home>
+  
+  Two-Way Data Binding
+  <p> Two-Way </p>
+  <input type="text" [(ngModel)]="name">
+  <hr>
+  <p>{{name}}
+  `,*/
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'proj1';
+  title = 'Project1';
+  name: string; 
 }
